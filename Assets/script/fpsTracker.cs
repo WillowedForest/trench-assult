0993688fdc86f9f5b8dc229ce1c5cd985b715eee
WillowedForest <joe.phillips935@proton.me>
2026-02-10 15:46:36 +0000

    Add baked lightmaps and scene updates
    Add baked lightmap and reflection probe assets for SampleScene and update the scene to reference the new LightingData asset. Apply numerous GameObject transform adjustments (scene layout tweaks), update the NavMesh surface asset and DefaultVolumeProfile, and modify ProjectSettings (DynamicsManager, NavMeshAreas, ProjectSettings.asset). Add fpsTracker.cs and attach it to the Camera, and add trench-assult.slnx.

---
+++ Assets/script/fpsTracker.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.IO;
+
+public class fpsTracker : MonoBehaviour
+{
+
+    // Update is called once per frame
+    int m_frameCounter = 0;
+    float m_timeCounter = 0.0f;
+    float m_lastFramerate = 0.0f;
+    public float m_refreshTime = 0.5f;
+
+    private StreamWriter sw; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (m_timeCounter < m_refreshTime)
+        {
+            m_timeCounter += Time.deltaTime;
+            m_frameCounter++;
+        }
+        else
+        {
+            //This code will break if you set your m_refreshTime to 0, which makes no sense.
+            m_lastFramerate = (float)m_frameCounter / m_timeCounter;
+            sw = new StreamWriter("test.txt");
+            sw.WriteLine(m_lastFramerate.ToString());
+            sw.Close();
+            m_frameCounter = 0;
+            m_timeCounter = 0.0f;
+        }
+    }
+}
